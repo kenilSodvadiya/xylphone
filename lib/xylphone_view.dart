d58222912048c25a363df89d2808530d78918e5f
kenilSodvadiya <kenilsodvadiya9653@gmail.com>
2023-01-05 18:19:49 +0530

    Initiale

---
+++ lib/xylphone_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class XylphoneView extends StatefulWidget {
+  const XylphoneView({super.key});
+
+  @override
+  State<XylphoneView> createState() => _XylphoneViewState();
+}
+
+class _XylphoneViewState extends State<XylphoneView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
